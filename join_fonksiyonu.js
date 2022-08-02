1e50672f31b7b31b5a863745bc98694b8533433d
ibrahim ethem ispir <ethem6046@gmail.com>
2022-08-02 21:31:28 +0300

    join fonksiyonu

---
+++ join_fonksiyonu.js
@@ -0,0 +1,12 @@
+// - Join - Fonksiyonu
+// Verilen dize içerisindeki
+// elemanları istenilen karakter
+// ile yada virgüllerden ayırır
+// ve bize tekrar döner
+
+const names = [ 
+  "ethem", "mustafa", "osman", "eren" 
+]
+
+const a = names.join(" - ")
+console.log(a);
