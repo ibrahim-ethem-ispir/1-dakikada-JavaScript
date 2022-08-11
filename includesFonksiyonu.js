510986828e0169025e9bc0c5f84d362d472724c4
ibrahim ethem ispir <ethem6046@gmail.com>
2022-08-11 20:35:53 +0300

    includes fonksiyonu

---
+++ includesFonksiyonu.js
@@ -0,0 +1,11 @@
+// - Includes fonksiyonu - 
+// dize içerisinde verilen 
+// değerin olup olmadığını 
+// kontrol eder. Sonuç true 
+// veya false döner.
+
+const arr = [101, 105, 107, 109, 111]
+
+const b = arr.includes(125)
+
+console.log(b);
