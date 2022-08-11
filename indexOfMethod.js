4d2339988db5de32c66eff644b2726d2e50ab97b
ibrahim ethem ispir <ethem6046@gmail.com>
2022-08-11 21:41:31 +0300

    indexOf Method

---
+++ indexOfMethod.js
@@ -0,0 +1,14 @@
+// - indexOf - Metodu bir 
+// dizi içerisinde sizin 
+// aradığınız eleman varsa 
+// elemanın index numarasını 
+// yoksa -1 döner.
+
+const arr = [
+  101, 105, 107, 109, 111,
+  275, 297, 309, 405, 407, 
+]
+
+const b = arr.indexOf(685)
+
+console.log(b);
