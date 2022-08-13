9e2fbbb8dc43d95d78f22b70a150f846a733ec2d
ibrahim ethem ispir <ethem6046@gmail.com>
2022-08-13 07:45:58 +0300

    push method

---
+++ pushMethod.js
@@ -0,0 +1,12 @@
+// - Push Method - 
+// array in sonuna bir 
+// veya daha fazla 
+// eleman eklemeye yarar.
+
+const arr = [
+    "a", "b", "c"
+];
+console.log(arr);
+
+arr.push("d", "e", "f")
+console.log(arr);
