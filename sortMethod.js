78c51443851395c158633ac2b4d90c7b599f4fde
ibrahim ethem ispir <ethem6046@gmail.com>
2022-08-21 19:09:03 +0300

    sort method

---
+++ sortMethod.js
@@ -0,0 +1,11 @@
+// - Sort Method - 
+// sort metodu verilen 
+// array'i sıralama yapar.
+
+const arr = [34, 12, 9, 5, 1, 37];
+const arr2 = ["g", "e", "b", "s", "t", "a"];
+
+const a = arr.sort((a, b) => a - b)
+
+console.log(a)
+console.log(arr2.sort())
