74d718a60bc2569bdafe753d54d6dc767d68f4be
ibrahim ethem ispir <ethem6046@gmail.com>
2022-08-30 12:01:44 +0300

    pop medhot

---
+++ popMethod.js
@@ -0,0 +1,13 @@
+// - Pop Method -
+// Verilen dizinin son 
+// elemanını atar ve dizi 
+// uzunluğu 1 azalır.
+
+const arr = [1,2,3,4,5]
+
+arr.pop()
+
+console.log(arr);
+
+arr.pop()
+console.log(arr);
