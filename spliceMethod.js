2e23f6426a9ee468099ddce56d03568bc6ed09b3
ibrahim ethem ispir <ethem6046@gmail.com>
2022-08-30 12:37:40 +0300

    splice method

---
+++ spliceMethod.js
@@ -0,0 +1,10 @@
+// - Splice Method -
+// Verilen dizi elemanlarını
+// index numaraları yardımı 
+// ile kaldırmaya veya 
+// değiştirmeye yarar.
+
+const arr = [1,2,3,4,5]
+
+arr.splice(1,3, "test")
+console.log(arr);
