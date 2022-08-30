859abd25c4b7853c069d9dfe15a97ef159a8737a
ibrahim ethem ispir <ethem6046@gmail.com>
2022-08-30 11:34:48 +0300

    reverse method

---
+++ reverseMethod.js
@@ -0,0 +1,17 @@
+// - Reverse Method -
+// Reverse Metodu verilen dizi 
+// elemanlarını sıralamasını 
+// tersine çevirir.
+
+const arr = [1, 2, 3, 4, 5, 6]
+const arr2 = ["d", "c", "b", "a"]
+
+const myString = "a.b.c.d.e"
+
+console.log(arr.reverse())
+console.log(arr2.reverse())
+
+const a = 
+myString.split(".").reverse()[0]
+
+console.log(a);
