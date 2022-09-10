ee3a559a02473af0ab727a97707b9b72c26ae472
ibrahim ethem ispir <ethem6046@gmail.com>
2022-09-10 15:49:40 +0300

    unshift method add

---
+++ unshiftMethod.js
@@ -0,0 +1,11 @@
+// - unshift Method -
+// Verilen dizinin başına 
+// bir veya birden fazla
+// eleman eklemeye yarar ve
+// dizi uzunluğunu döndürür.
+
+const arr = [
+    "a","b","c","d"
+]
+console.log(arr.unshift("y"))
+console.log(arr);
