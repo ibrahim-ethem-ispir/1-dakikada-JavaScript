9fbc9e20c09d4498eb234630ae154127c2cdb2d2
ibrahim ethem ispir <ethem6046@gmail.com>
2022-09-10 18:05:30 +0300

    slice method add

---
+++ sliceMethod.js
@@ -0,0 +1,10 @@
+// - Slice Method -
+// dizinin verilen başlangıç ve bitiş indexsine göre dizi içerisindeki seçilen elemanları alır ve yeni bir dizi olarak bize döner.
+// başlangıç index 'i dahil bitiş index 'i dahil değildir.
+
+const arr = [
+  13,27,33,46,54,67,72,89
+]
+const a = 
+    arr.slice(2,6)
+console.log(a);
